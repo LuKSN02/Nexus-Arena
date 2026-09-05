d4ec9887be10783bb1b700b3641141014bfd9bff
LuLu432 <trajano.neves01@gmail.com>
2026-09-05 13:51:22 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    disco (file://). Veja o README para como rodar um servidor local.
    ========================================================================== */
 
-const CACHE_VERSION = 'nexus-arena-v8';
+const CACHE_VERSION = 'nexus-arena-v9';
 
 const APP_SHELL = [
   './',
